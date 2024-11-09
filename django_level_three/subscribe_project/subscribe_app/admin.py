9233ca7220f7c493a7ca0eeb0381ecd663960524
Pebri Andika <154705202+WhatZitTooya28@users.noreply.github.com>
2024-11-09 22:37:33 +0700

    Delete django_level_three directory

--- django_level_three/subscribe_project/subscribe_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from subscribe_app.models import Customer
-
-# Register your models here.
-admin.site.register(Customer)
--- django_level_three/subscribe_project/subscribe_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from subscribe_app.models import Customer
-
-class NewSubscriber(forms.ModelForm):
-    class Meta():
-        model = Customer
-        fields = '__all__'
